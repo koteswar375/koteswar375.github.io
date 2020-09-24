8f1bd2fa853c093a4d72fc789a6f2c65b26f2ff7
Koteswarrao375 <koteswar.meesala.ext@siemens.com>
2020-09-24 23:20:34 +0530

    Header made responsive

--- src/App.js
+++ src/App.js
@@ -4,14 +4,25 @@ import Header from './Components/Header/Header';
 import Intro from './Components/Intro/Intro';
 import AboutMe from './Components/AboutMe/AboutMe';
 import Content from './Components/Content/Content';
+import Contact from './Components/Contact/Contact';
+import { Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-        <Header></Header>
-        <Intro></Intro>
-        <AboutMe></AboutMe>
-        <Content></Content>
+      <Header></Header>
+      <Switch>
+        <Route exact path="/">
+          <Intro></Intro>
+          <AboutMe></AboutMe>
+          <Content></Content>
+        </Route>
+        <Route path="/contact">
+          <Contact/>
+        </Route>
+      </Switch>
+
+
     </div>
   );
 }
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,18 +1,31 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './Header.css';
+import { Link } from 'react-router-dom';
+import * as M from 'materialize-css';
 
-const Header = (props) => {
+class Header extends Component {
+  
+    componentDidMount() {
+        M.AutoInit();
+    }
 
+    render() {
+        return (
+            <div className="nav-wrapper">
+                <div data-target="mobile-demo" className="sidenav-trigger hide-on-large-only"><i className="material-icons">menu</i></div>
+                <div className="header grey darken-3  white-text row hide-on-med-and-down">
+                    <div className="title col s12 m12 l4"> <Link to="/">KOTESWAR RAO MEESALA</Link></div>
+                    <Link to="/resume" className="col s12 m6 l1 offset-l6">Resume</Link>
+                    <Link to="/contact" className="col s12 m6 l1">Contact</Link>
 
-    return (
-        <div className="header grey darken-3  white-text">
-                <span className="title">KOTESWAR RAO MEESALA</span>
-                <div className="header-items">
-                    <span className="headerItem">Contact</span>
-                    <span className="headerItem">Resume</span>
                 </div>
-        </div>
-    )
+                <ul className="sidenav" id="mobile-demo">
+                    <li><Link to="/resume">Resume</Link></li>
+                    <li><Link to="/contact">Contact</Link></li>
+                </ul>
+            </div>
+        )
+    }
 }
 
 export default Header;
