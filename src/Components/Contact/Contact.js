32a42ecb42886250000a250a66c5f6eff30e9179
Koteswarrao375 <koteswar.meesala.ext@siemens.com>
2020-09-27 12:34:29 +0530

    image deleted

---
+++ src/Components/Contact/Contact.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+
+class Contact extends Component {
+    state = {
+
+    }
+
+    render() {
+        return (
+            <div>Contact me at koteswar.meesala@gmail.com</div>
+        )
+    }
+}
+
+
+export default Contact;
