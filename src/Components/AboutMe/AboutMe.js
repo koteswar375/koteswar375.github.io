1a5a330fc25c399532d84274304b1bc0d5ad403b
koti meesala <kotislr75@gmail.com>
2021-12-11 01:01:48 -0600

    changed the background

--- src/Components/AboutMe/AboutMe.js
+++ src/Components/AboutMe/AboutMe.js
@@ -6,8 +6,8 @@ const AboutMe = (props) => {
 
     return (
         <div className="aboutme  row center-align">
-                <h5 className="white-text col l12">ABOUT ME</h5>
-                <p className="white-text col l6 m8">I am a passionate software developer with five years experience in frontend, backend frameworks, libraries, tools to develop web apps that scale across users. </p>
+                <h5 className="col l12">ABOUT ME</h5>
+                <p className="col l6 m8">I am a passionate software developer with five years experience in frontend, backend frameworks, libraries, tools to develop web apps that scale across users. </p>
         </div>
     )
 }
--- src/Components/Intro/Intro.js
+++ src/Components/Intro/Intro.js
@@ -6,7 +6,7 @@ import profilePic from '../../assets/dp.png';
 const Intro = (props) => {
     return (
         <div className="intro">
-            <div className="job-title center-align white-text">FULL STACK DEVELOPER</div>
+            <div className="job-title center-align">FULL STACK DEVELOPER</div>
             <div className="profile-pic">
                 <img alt="Profile" src={profilePic}/>
             </div>
