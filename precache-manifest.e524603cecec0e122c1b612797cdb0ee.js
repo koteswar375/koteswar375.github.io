047c9c877335d22a8c5b7617e1b24759460a9bcd
koti meesala <kotislr75@gmail.com>
2021-12-11 01:33:35 -0600

    Updates

---
+++ precache-manifest.e524603cecec0e122c1b612797cdb0ee.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abda143e0b77b2de3b418a9a64657a3d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "906dc4bc55b6128917ff",
+    "url": "/static/css/2.a389d3b1.chunk.css"
+  },
+  {
+    "revision": "c606a6a1c4171fea50a8",
+    "url": "/static/css/main.a3dcdc84.chunk.css"
+  },
+  {
+    "revision": "906dc4bc55b6128917ff",
+    "url": "/static/js/2.bb282133.chunk.js"
+  },
+  {
+    "revision": "0ea762cc4074de3a598b2f7dc8894b4c",
+    "url": "/static/js/2.bb282133.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c606a6a1c4171fea50a8",
+    "url": "/static/js/main.5bbf645d.chunk.js"
+  },
+  {
+    "revision": "540d812b028fd61de1d7",
+    "url": "/static/js/runtime-main.31af67fe.js"
+  },
+  {
+    "revision": "3e3291315be8a541d2615d1a164033a8",
+    "url": "/static/media/dp.3e329131.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e1886c55a2b54faf0622e447337988bd.js"
+  "/precache-manifest.e524603cecec0e122c1b612797cdb0ee.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
